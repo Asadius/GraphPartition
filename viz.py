6ec0ce4b680aa9dca992b628701c0449efd62118
Asadius <ajamil@colgate.edu>
2019-06-03 21:37:42 -0400

    3rd version - review logic

--- viz.py
+++ viz.py
@@ -1,20 +1,24 @@
 import networkx as nx
 import metis
 import math
-import pydot
 import pygraphviz as pgv
 
 def graphPartition(G, clusterSize):
-    numPartitions = math.ceil(G.size()/float(clusterSize)) # heuristic for # of clusters given approximately equal cluster size
-    (edgecuts, parts) = metis.part_graph(G, int(numPartitions)) # k-way cut algorithm
-    colors = ['red', 'blue', 'green', 'orange', 'yellow', 'purple', 'black']
+    numClusters = math.ceil(len(G)/float(clusterSize)) # heuristic for # of clusters given approximately equal cluster size
+    print numClusters
+    (edgecuts, parts) = metis.part_graph(G, int(numClusters)) # k-way cut algorithm
+    colors = ['red', 'blue', 'green', 'orange', 'yellow', 'purple', 'black', 'maroon', 'brown', 'indigo', 'cyan']
     for i, p in enumerate(parts):
+        #print i, p
         G.node[i]['color'] = colors[p]
     nx.drawing.nx_pydot.write_dot(G, 'example.dot')  # writes graph partition into dot file format
     result = pgv.AGraph('example.dot')  # load dot format representation of graph to create visualization
+    result.node_attr.update(shape='circle')
+    result.node_attr.update(label=' ')
+    result.node_attr.update(style='filled')
     result.layout()  # default layout
     result.draw('example.svg')  # writes visualization as svg file
     print("Wrote example.svg")
 
 
-graphPartition(metis.example_networkx(), 6)
+graphPartition(nx.circular_ladder_graph(25), 10)
